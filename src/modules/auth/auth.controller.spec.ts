3f881a6a2b8dbc0b400c28c284068ff293b92035
Vinicius Inocêncio <vicnicius2009@gmail.com>
2023-12-07 14:25:23 -0300

    Test: adding auth route tests

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,5 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { AuthenticationGuard } from './guards/authentication.guard';
+import { AuthorizationGuard } from './guards/authorization.guard';
+import { Role as RoleType } from '@prisma/client';
+import { randomUUID } from 'crypto';
+import { UserEntity } from '../user/entities/user.entity';
+
+const userId = randomUUID();
+
+const mockUser: UserEntity = {
+  id: userId,
+  email: 'test@example.com',
+  password: 'hashedPassword',
+  role: 'CUSTOMER',
+  username: 'test',
+  createdAt: new Date('2021-01-01T11:00:00.000Z'),
+  updatedAt: new Date('2021-01-01T11:00:00.000Z'),
+};
+
+// Mock AuthService
+const authServiceMock = {
+  login: jest.fn(),
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,12 +31,58 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-    }).compile();
+      providers: [{ provide: AuthService, useValue: authServiceMock }],
+    })
+      .overrideGuard(AuthenticationGuard)
+      .useValue({ canActivate: jest.fn(() => true) }) // Mocking AuthenticationGuard
+      .overrideGuard(AuthorizationGuard)
+      .useValue({ canActivate: jest.fn(() => true) }) // Mocking AuthorizationGuard
+      .compile();
 
     controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should return the result from authService.login', async () => {
+      const loginDto: LoginDto = {
+        email: 'test@example.com',
+        password: 'password123',
+      };
+      authServiceMock.login.mockReturnValue({ access_token: 'mockedToken' });
+
+      const result = await controller.login(loginDto);
+
+      expect(result).toEqual({ access_token: 'mockedToken' });
+      expect(authServiceMock.login).toHaveBeenCalledWith(
+        loginDto.email,
+        loginDto.password,
+      );
+    });
+  });
+
+  describe('getProfile', () => {
+    it('should return the user from the request object', () => {
+      const mockRequest = { user: mockUser };
+
+      const result = controller.getProfile(mockRequest);
+
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should have the @UseGuards decorators and @Role decorator', () => {
+      const metadata = Reflect.getMetadata('role', controller.getProfile);
+      const useGuardsMetadata = Reflect.getMetadata(
+        '__guards__',
+        controller.getProfile,
+      );
+
+      expect(metadata).toEqual(RoleType.ADMIN);
+      expect(useGuardsMetadata).toContain(AuthenticationGuard);
+      expect(useGuardsMetadata).toContain(AuthorizationGuard);
+    });
   });
 });
--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,18 +1,79 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { NotFoundException, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { randomUUID } from 'crypto';
+import { UserEntity } from '../user/entities/user.entity';
+
+const userId = randomUUID();
+
+const mockUser: UserEntity = {
+  id: userId,
+  email: 'test@example.com',
+  password: 'hashedPassword',
+  role: 'CUSTOMER',
+  username: 'test',
+  createdAt: new Date('2021-01-01T11:00:00.000Z'),
+  updatedAt: new Date('2021-01-01T11:00:00.000Z'),
+};
+
+const userServiceMock = {
+  findByEmail: jest.fn(),
+};
+
+const jwtServiceMock = {
+  signAsync: jest.fn(() => 'mockedToken'),
+};
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UserService, useValue: userServiceMock },
+        { provide: JwtService, useValue: jwtServiceMock },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should throw NotFoundException when user is not found', async () => {
+      userServiceMock.findByEmail.mockResolvedValue(null);
+
+      await expect(
+        authService.login('nonexistent@example.com', 'password'),
+      ).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw UnauthorizedException when password is invalid', async () => {
+      userServiceMock.findByEmail.mockResolvedValue(mockUser);
+      jest.spyOn(bcrypt, 'compare').mockResolvedValue(false as never);
+
+      await expect(
+        authService.login('test@example.com', 'invalidPassword'),
+      ).rejects.toThrow(UnauthorizedException);
+    });
+
+    it('should return access_token when login is successful', async () => {
+      userServiceMock.findByEmail.mockResolvedValue(mockUser);
+      jest.spyOn(bcrypt, 'compare').mockResolvedValue(true as never);
+
+      const result = await authService.login(
+        'test@example.com',
+        'validPassword',
+      );
+
+      expect(result).toEqual({ access_token: 'mockedToken' });
+    });
   });
 });
